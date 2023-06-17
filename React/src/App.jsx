225b073a2fc5856bc692059d30b6be7223fadc88
SHIVAM SHARMA <128138567+shivamsharmadac@users.noreply.github.com>
2023-06-17 11:04:15 +0530

    second

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,8 +1,10 @@
 import MyBio from "./MyBio";
+import Heading from "./Heading";
 
 export default function App(){
   return (
     <>
+    <Heading text="Welcome"/>
     <MyBio  num={7} />
     <MyBio   />
     <MyBio  num={7} />
